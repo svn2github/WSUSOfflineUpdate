1d95eb7723c41cd14b480be3f8f60317afd55ccd
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2009-06-29 15:02:45 +0000

    - INSTALLATION HINT: In spite of the different directory name inside the archive, you still may unpack v. 6.0 into an existing structure, overwriting existing files - Download scripts for Linux added (Thanks to Tobias Breitling and Stefan Jöhnke) - USB stick option added to UpdateGenerator script (Thanks to Christian Riedel) - Service Packs 2 for Office 2007 and Office Compatibility Pack integrated - Optional installation of Windows PowerShell 1.0 on Windows XP, Server 2003 and Vista (x86/x64) systems - Microsoft Service Pack Uninstall Tool for the 2007 Microsoft Office Suite will be downloaded - DirectX End-User Runtimes updated (March 2009) - Fix: Internet Explorer 8 wasn't installed on automatic recall runs (Thanks to "NetZwerg") - Fix: Office 2000 Service Pack level was not properly detected (Thanks to Mark Gerber) - Fix: Office 2000 Service Release 1a (O2KSR1aDL.EXE) would not be installed (Thanks to Mark Gerber) - Fix: DownloadUpdates.cmd echoed and logged credentials for proxy access (Thanks to Jörg Watermann) - Fix: Displayed number of statically defined updates to be downloaded was incorrect (Thanks to "blind-fish") - Fix (defect #70): WSUS download errors caused re-downloading of existing updates (Thanks to "me") - Fix: Microsoft fixed broken URL for WindowsXP-Windows2000-Script56-KB917344-x86-hun.exe (Thanks to Rudolf Finszter) - Fix: Download exclude list for Windows 2000 updated to avoid windows2000-directx8* files - Fix: Sleep.vbs was not found on SafeRmDir.cmd calls in DownloadUpdates.cmd (Thanks to "siggibaer")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@3 c6bda2d4-11f8-444b-8fa8-e24aff79bece

---
+++ tags/ctupdate6.0/sh/CreateISOImage.sh
@@ -0,0 +1,163 @@
+#/bin/bash
+
+#set workdir
+cd $( dirname $0 )
+rm ../temp/ExcludeListISO*
+printusage()
+{
+echo ERROR Invalid parameter: $1 $2 $3 $4
+echo
+echo "Usage: $0 [system] [language] [parameter]"
+echo
+echo "Supported systems:"
+echo "w2k, wxp, wxp-x64, w2k3, w2k3-x64, w60, w60-x64, oxp, o2k, o2k3, o2k7, all-x64, all-x86"
+echo
+echo "Supported languages:"
+echo enu, deu, nld, esn, fra, ptg, ptb, ita, rus, plk, ell, csy
+echo dan, nor, sve, fin, jpn, kor, chs, cht, hun, trk, ara, heb
+echo
+echo "Parameter:"
+echo "/excludesp - exclude servicepacks"
+echo "/dotnet    - include .Net-Framework"
+echo 
+echo "Example: $0 wxp deu /dotnet"
+echo
+exit
+}
+
+printheader()
+{
+clear
+echo "**********************************************************"
+echo "***            c't Offline Update ISO-Maker            ***"
+echo "***                  for Linux Systems                 ***"
+echo "***                                                    ***"
+echo "***   http://www.heise.de/ct/projekte/offlineupdate/   ***"
+echo "***   Author: Stefan Joehnke                           ***"
+echo "**********************************************************"
+echo
+}
+
+printheader
+#check config
+X=`which mkisofs`
+Y=`which genisoimage`
+iso_tool=""
+if [ ! -x "$X" ] && [ ! -x "$Y" ]
+then
+ echo
+ echo Please install mkisofs.
+ echo
+ echo Command in debian:
+ echo apt-get install mkisofs
+ echo or
+ echo apt-get install genisoimage
+ echo
+ echo Command in Suse:
+ echo zypper install genisoimage
+ echo
+ exit
+fi
+if [ -x "$X" ]
+	then
+	iso_tool="mkisofs"
+	else
+	iso_tool="genisoimage"
+fi
+
+evaluateparams()
+{
+syslist=("w2k" "wxp" "wxp-x64" "w2k3" "w2k3-x64" "w60" "w60-x64" "oxp" "o2k" "o2k3" "o2k7" "all-x64" "all-x86")
+langlist=("enu" "deu" "nld" "esn" "fra" "ptg" "ptb" "ita" "rus" "plk" "ell" "csy" "dan" "nor" "sve" "fin" "jpn" "kor" "chs" "cht" "hun" "trk" "ara" "heb")
+EXCLUDE_SP="0"
+dotnet="0"
+
+ for i in ${syslist[@]}
+  do
+   if [ "$1" == "$i" ]
+    then
+     sys="$1"
+   fi
+ done
+ for i in ${langlist[@]}
+  do
+   if [ "$2" == "$i" ]
+    then
+     lang="$2"
+   fi
+  done
+ if [ "$sys" == "w60" -o "$sys" == "w60-x64" ]
+  then
+  	echo Setting language to glb...
+   lang="glb"
+ fi
+ if [ "$sys" == "" -o "$lang" == "" ]
+ 	then
+ 		printusage
+ fi
+ 
+ if [ "$3" == "/excludesp" ]
+  then
+   EXCLUDE_SP="1"
+ fi
+ if [ "$4" == "/excludesp" ]
+  then
+   EXCLUDE_SP="1"
+ fi
+ if [ "$3" == "/dotnet" ]
+  then
+   dotnet="1"
+ fi
+ if [ "$4" == "/dotnet" ]
+  then
+   dotnet="1"
+ fi
+}
+
+evaluateparams $1 $2 $3 $4
+
+echo "Creating ISO-Filter..."
+
+excludeiso1="../exclude/ExcludeListISO-$sys.txt"
+excludeiso2="../exclude/ExcludeListISO-$sys-x86.txt"
+
+echo "Writing builddate.txt..."
+date > ../client/builddate.txt
+
+if [ -f "$excludeiso1" ]
+	then
+		cp ../exclude/ExcludeListISO-$sys.txt ../temp/ExcludeListISO-$sys.txt
+fi
+if [ -f "$excludeiso2" ]
+	then
+		cp ../exclude/ExcludeListISO-$sys-x86.txt ../temp/ExcludeListISO-$sys.txt
+fi
+if [ "$EXCLUDE_SP" == "1" ]
+ 	then
+ 		cat ../exclude/ExcludeList-SPs.txt >> ../temp/ExcludeListISO-$sys.txt
+fi
+if [ "$dotnet" != "1" ]
+	then
+		echo "dotnet*" >> ../temp/ExcludeListISO-$sys.txt
+fi
+x=0
+langlist=("enu" "deu" "nld" "esn" "fra" "ptg" "ptb" "ita" "rus" "plk" "ell" "csy" "dan" "nor" "sve" "fin")
+for i in ${langlist[@]}
+	do
+		if [ "${langlist[x]}" == "enu" ]
+			then
+				echo ${langlist[x]}* | grep -v $lang >> ../temp/ExcludeListISO-$sys.txt
+			else
+				echo *${langlist[x]}* | grep -v $lang >> ../temp/ExcludeListISO-$sys.txt
+		fi
+		x=$x+1
+done
+cp ../temp/ExcludeListISO-$sys.txt ../temp/ExcludeListISOtmp-$sys.txt
+cat ../temp/ExcludeListISOtmp-$sys.txt | grep "\/" | sed 's/\\\/\*//' | sed 's;*;;' >> ../temp/ExcludeListISO-$sys.txt
+rm ../temp/ExcludeListISOtmp-$sys.txt
+
+echo "Creating ISO image for $sys $lang..."
+$iso_tool -iso-level 4 -joliet -joliet-long -rational-rock -exclude-list ../temp/ExcludeListISO-$sys.txt -output ../iso/wsusoffline-$sys-$lang.iso -volid ctou_$sys_$lang ../client
+rm ../client/builddate.txt
+echo "done."
+
