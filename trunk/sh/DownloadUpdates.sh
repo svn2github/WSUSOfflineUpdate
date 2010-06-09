ab18fc654091c1a68edd2eb985632f3d394dacc2
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2010-06-09 13:53:17 +0000

    - Office updates up to 06/2010 integrated by static definitions
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@112 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -3,7 +3,7 @@
 ##########################################################
 ###           WSUS Offline Update Downloader           ###
 ###                  for Linux systems                 ###
-###                   v. 6.51+ (r111)                  ###
+###                   v. 6.51+ (r112)                  ###
 ###                                                    ###
 ###   http://www.wsusoffline.net/                      ###
 ###   Authors: Tobias Breitling, Stefan Joehnke,       ###
@@ -395,7 +395,7 @@ cat << END
 **********************************************************
 ***           WSUS Offline Update Downloader           ***
 ***                  for Linux systems                 ***
-***                   v. 6.51+ (r111)                  ***
+***                   v. 6.51+ (r112)                  ***
 ***                                                    ***
 ***   http://www.wsusoffline.net/                      ***
 ***   Authors: Tobias Breitling, Stefan Joehnke,       ***
