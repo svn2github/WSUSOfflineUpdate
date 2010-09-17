9ffe13e0380244a9af2cca469f09349747f93840
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2010-09-17 19:33:03 +0000

    - Fix: Download exclude list for Windows XP (ExcludeList-wxp-x86.txt) erroneously excluded kb923561 - Fix: Office XP patches kb975008 and kb2293422 weren't downloaded as the required language specific versions (Thanks to "s.a.h.")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@143 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- branches/w2k_ofc_construction_site/sh/DownloadUpdates.sh
+++ branches/w2k_ofc_construction_site/sh/DownloadUpdates.sh
@@ -3,7 +3,7 @@
 ##########################################################
 ###           WSUS Offline Update Downloader           ###
 ###                  for Linux systems                 ###
-###    v. 6.6.3+ (w2k_ofc_construction_site (r141))    ###
+###    v. 6.6.3+ (w2k_ofc_construction_site (r143))    ###
 ###                                                    ###
 ###   http://www.wsusoffline.net/                      ###
 ###   Authors: Tobias Breitling, Stefan Joehnke,       ###
@@ -395,7 +395,7 @@ cat << END
 **********************************************************
 ***           WSUS Offline Update Downloader           ***
 ***                  for Linux systems                 ***
-***    v. 6.6.3+ (w2k_ofc_construction_site (r141))    ***
+***    v. 6.6.3+ (w2k_ofc_construction_site (r143))    ***
 ***                                                    ***
 ***   http://www.wsusoffline.net/                      ***
 ***   Authors: Tobias Breitling, Stefan Joehnke,       ***
