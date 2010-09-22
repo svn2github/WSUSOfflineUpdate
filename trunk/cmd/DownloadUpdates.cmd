6c24daa93ee3f9d199d83b1baa4ccecd73389296
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2010-09-22 19:22:52 +0000

    - Fix: Office 2003 patch kb2293428 wasn't downloaded as the required language specific version
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@146 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/cmd/DownloadUpdates.cmd
+++ trunk/cmd/DownloadUpdates.cmd
@@ -10,7 +10,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 %~d0
 cd "%~p0"
 
-set WSUSOFFLINE_VERSION=6.6.3+ (r145)
+set WSUSOFFLINE_VERSION=6.6.3+ (r146)
 set DOWNLOAD_LOGFILE=..\log\download.log
 title %~n0 %1 %2
 echo Starting WSUS Offline Update download (v. %WSUSOFFLINE_VERSION%) for %1 %2...
