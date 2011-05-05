36450974db8742336e2ea793aa2780c31e311edb
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2011-05-05 08:00:40 +0000

    - Prepare release
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@244 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/client/UpdateInstaller.au3
+++ trunk/client/UpdateInstaller.au3
@@ -1,11 +1,11 @@
-; *** WSUS Offline Update 6.8.2 - Installer ***
+; *** WSUS Offline Update 6.8.3 - Installer ***
 ; ***      Author: T. Wittrock, Kiel        ***
 ; ***  Dialog scaling added by Th. Baisch   ***
 
 #include <GUIConstants.au3>
 #RequireAdmin
 
-Dim Const $caption                    = "WSUS Offline Update 6.8.2 - Installer"
+Dim Const $caption                    = "WSUS Offline Update 6.8.3 - Installer"
 
 ; Registry constants
 Dim Const $reg_key_wsh_hklm           = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows Script Host\Settings"
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -3,7 +3,7 @@
 ##########################################################
 ###           WSUS Offline Update Downloader           ###
 ###                  for Linux systems                 ###
-###                   v. 6.8.2+ (r243)                 ###
+###                      v. 6.8.3                      ###
 ###                                                    ###
 ###   http://www.wsusoffline.net/                      ###
 ###   Authors: Tobias Breitling, Stefan Joehnke,       ###
@@ -392,7 +392,7 @@ cat << END
 **********************************************************
 ***           WSUS Offline Update Downloader           ***
 ***                  for Linux systems                 ***
-***                   v. 6.8.2+ (r243)                 ***
+***                      v. 6.8.3                      ***
 ***                                                    ***
 ***   http://www.wsusoffline.net/                      ***
 ***   Authors: Tobias Breitling, Stefan Joehnke,       ***
