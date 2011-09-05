886dfe7efbcd8dfa5631e2579d7dce3cec217170
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2011-09-05 14:21:58 +0000

    - Fix: Excluded kb810217 for Windows XP due to mismatching SHA-1 message digest (Thanks to "boco")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@286 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -3,7 +3,7 @@
 ##########################################################
 ###           WSUS Offline Update ISO maker            ###
 ###                  for Linux systems                 ###
-###                    v. 6.9+ (r285)                  ###
+###                    v. 6.9+ (r286)                  ###
 ###                                                    ###
 ###   http://www.wsusoffline.net/                      ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg      ###
@@ -47,7 +47,7 @@ cat << END
 **********************************************************
 ***           WSUS Offline Update ISO maker            ***
 ***                  for Linux systems                 ***
-***                    v. 6.9+ (r285)                  ***
+***                    v. 6.9+ (r286)                  ***
 ***                                                    ***
 ***   http://www.wsusoffline.net/                      ***
 ***   Authors: Stefan Joehnke, Walter Schiessberg      ***
