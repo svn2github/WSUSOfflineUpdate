28a02422f7a803b7bfe369da8074359791ce9a8f
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2011-09-07 15:07:43 +0000

    - Fix: Download exclude lists for Windows 7 / Server 2008 R2 (x86/x64) modified since Microsoft updated their list of updates included in SP1 (see http://www.microsoft.com/download/en/details.aspx?displaylang=en&id=269) on March 24th, 2011
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@287 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/cmd/DownloadUpdates.cmd
+++ trunk/cmd/DownloadUpdates.cmd
@@ -9,7 +9,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=6.9+ (r286)
+set WSUSOFFLINE_VERSION=6.9+ (r287)
 set DOWNLOAD_LOGFILE=..\log\download.log
 title %~n0 %1 %2 %3 %4 %5 %6 %7 %8 %9
 echo Starting WSUS Offline Update download (v. %WSUSOFFLINE_VERSION%) for %1 %2...
