45f941427c1cfd607595699d79275cd6c753e437
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2011-12-06 11:53:04 +0000

    - Office File Converter/Compatibility Packs and Office File Validation Add-In may now be installed separately - Installation routine will now adjust power management settings when running under WOUTempAdmin user account to avoid standby and hibernate modes - DownloadUpdates.cmd script now supports "/skipdownload" switch for "determine only" mode - DownloadUpdates.cmd switches "/skipdownload" and "/skipdynamic" may be preset in UpdateGenerator.ini file - Installation exclude list (...\client\exclude\ExcludeList.txt) may contain comma separated descriptions (Thanks to "Pete Monger") - DoUpdate.cmd script won't terminate any longer if most recent OS Service Pack is not found - Fix: Linux script DownloadUpdates.sh did not download C++ Runtime Libraries (Thanks to "Spider-Mann") - Fix: DownloadUpdates.cmd showed misleading warnings when attempting to remove alternate data streams on non-NTFS file systems (Thanks to "aker") - Fix: Linux script DownloadUpdates.sh missed grep -F option ('fixed strings') several times (Thanks to "zoc") - Fix: When running under WOUTempAdmin user account, DoUpdate.cmd script will now assume that the service 'automatic updates' (wuauserv) is running if its start mode is 'Auto' (Thanks to "1inux") - Fix: Office File Validation installation now includes update kb2553065 (Thanks to "harry" and "-Iwan-") - Fix: Automatic recall failed if script path contained a blank (Thanks to "abox")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@324 c6bda2d4-11f8-444b-8fa8-e24aff79bece

---
+++ tags/wsusoffline7.2/client/cmd/ListUpdatesToInstall.cmd
@@ -0,0 +1,146 @@
+@echo off
+rem *** Author: T. Wittrock, Kiel ***
+
+verify other 2>nul
+setlocal enableextensions
+if errorlevel 1 goto NoExtensions
+
+if "%UPDATE_LOGFILE%"=="" set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
+
+if "%TEMP%"=="" goto NoTemp
+pushd "%TEMP%"
+if errorlevel 1 goto NoTempDir
+popd
+if "%OS_NAME%"=="" goto NoOSName
+if "%OS_LANG%"=="" goto NoOSLang
+if "%OS_ARCH%"=="" goto NoOSArch
+
+:EvalParams
+if "%1"=="" goto NoMoreParams
+if /i "%1"=="/excludestatics" set EXC_STATICS=1
+shift /1
+goto EvalParams
+
+:EvalStatics
+if exist %1 (
+  if exist "%TEMP%\InstalledUpdateIds.txt" (
+    %SystemRoot%\system32\findstr.exe /L /I /V /G:"%TEMP%\InstalledUpdateIds.txt" %1 >>"%TEMP%\MissingUpdateIds.txt"
+  ) else (
+    for /F %%i in (%1) do echo %%i>>"%TEMP%\MissingUpdateIds.txt"
+  )
+)
+goto :eof
+
+:NoMoreParams
+rem *** Add statically defined update ids ***
+if "%EXC_STATICS%"=="1" goto ExcludeStatics
+if exist ..\static\StaticUpdateIds-%OS_NAME%-%OS_ARCH%.txt call :EvalStatics ..\static\StaticUpdateIds-%OS_NAME%-%OS_ARCH%.txt
+if exist ..\static\custom\StaticUpdateIds-%OS_NAME%-%OS_ARCH%.txt call :EvalStatics ..\static\custom\StaticUpdateIds-%OS_NAME%-%OS_ARCH%.txt
+if exist ..\static\StaticUpdateIds-%OFC_NAME%.txt call :EvalStatics ..\static\StaticUpdateIds-%OFC_NAME%.txt
+if exist ..\static\custom\StaticUpdateIds-%OFC_NAME%.txt call :EvalStatics ..\static\custom\StaticUpdateIds-%OFC_NAME%.txt
+
+:ExcludeStatics
+rem *** List update files ***
+if exist "%TEMP%\InstalledUpdateIds.txt" del "%TEMP%\InstalledUpdateIds.txt"
+if not exist "%TEMP%\MissingUpdateIds.txt" goto EoF
+if exist "%TEMP%\UpdatesToInstall.txt" del "%TEMP%\UpdatesToInstall.txt"
+if exist ..\exclude\ExcludeList.txt copy /Y ..\exclude\ExcludeList.txt "%TEMP%\ExcludeList.txt" >nul
+if exist ..\exclude\custom\ExcludeList.txt (
+  for /F "tokens=*" %%i in (..\exclude\custom\ExcludeList.txt) do echo %%i>>"%TEMP%\ExcludeList.txt"
+)
+for /F "usebackq tokens=1,2 delims=," %%i in ("%TEMP%\MissingUpdateIds.txt") do (
+  if exist "%TEMP%\Update.txt" del "%TEMP%\Update.txt"
+  %SystemRoot%\system32\find.exe /I "%%i" "%TEMP%\ExcludeList.txt" >nul 2>&1
+  if errorlevel 1 (
+    for %%k in (%OS_NAME%-%OS_ARCH% %OS_NAME% win) do (
+      for %%l in (%OS_LANG% glb) do (
+        call ListUpdateFile.cmd ie%IE_VER_MAJOR%-*%%i ..\%%k\%%l
+        call ListUpdateFile.cmd windowsmedia%WMP_VER_MAJOR%-*%%i ..\%%k\%%l
+        call ListUpdateFile.cmd windowsmedia-*%%i ..\%%k\%%l
+        call ListUpdateFile.cmd mdac%MDAC_VER_MAJOR%%MDAC_VER_MINOR%-*%%i ..\%%k\%%l
+        call ListUpdateFile.cmd windows2000*%%i ..\%%k\%%l /searchleftmost
+        call ListUpdateFile.cmd windowsxp*%%i ..\%%k\%%l /searchleftmost
+        call ListUpdateFile.cmd windowsserver2003*%%i-js%WSH_VER_MAJOR%%WSH_VER_MINOR% ..\%%k\%%l /searchleftmost
+        call ListUpdateFile.cmd windowsserver2003*%%i ..\%%k\%%l /searchleftmost
+        call ListUpdateFile.cmd windows6*%%i ..\%%k\%%l /searchleftmost
+        call ListUpdateFile.cmd windows*%%i ..\%%k\%%l /searchleftmost
+        call ListUpdateFile.cmd %%i ..\%%k\%%l
+      )
+    )
+    call ListUpdateFile.cmd ndp*%%i*-%OS_ARCH% ..\dotnet\%OS_ARCH%-glb /searchleftmost
+    for %%k in (%OFC_LANG% glb) do (
+      if exist ..\ofc\UpdateTable-ofc-%%k.csv (
+        if not exist "%TEMP%\Update.txt" (
+          for /F "tokens=1,2 delims=," %%l in (..\ofc\UpdateTable-ofc-%%k.csv) do (
+            if "%%l"=="%%j" (
+              if exist "%TEMP%\Update.txt" del "%TEMP%\Update.txt"
+              call ListUpdateFile.cmd %%m ..\ofc\%%k /searchleftmost
+            )
+          )
+        )
+      )
+    )
+    if not exist "%TEMP%\Update.txt" (
+      for %%k in (%OFC_NAME% ofc o2k3 o2k7 o2k10) do (
+        for %%l in (%OFC_LANG% glb) do (
+          call ListUpdateFile.cmd %%i ..\%%k\%%l
+        )
+      )
+    )
+    if exist "%TEMP%\Update.txt" (
+      del "%TEMP%\Update.txt"
+    ) else (
+      echo Warning: Update KB%%i not found.
+      echo %DATE% %TIME% - Warning: Update KB%%i not found >>%UPDATE_LOGFILE%
+    )
+  ) else (
+    for /F "tokens=1* delims=,;" %%k in ('%SystemRoot%\system32\findstr.exe /I "%%i" "%TEMP%\ExcludeList.txt"') do (
+      if "%%l"=="" (
+        echo Info: Skipping update %%k due to matching black list entry.
+        echo %DATE% %TIME% - Info: Skipped update %%k due to matching black list entry >>%UPDATE_LOGFILE%
+      ) else (
+        echo Info: Skipping update %%k ^(%%l^) due to matching black list entry.
+        echo %DATE% %TIME% - Info: Skipped update %%k ^(%%l^) due to matching black list entry >>%UPDATE_LOGFILE%
+      )
+    )
+  )
+)
+del "%TEMP%\MissingUpdateIds.txt"
+del "%TEMP%\ExcludeList.txt"
+goto EoF
+
+:NoExtensions
+echo ERROR: No command extensions available.
+goto Error
+
+:NoTemp
+echo ERROR: Environment variable TEMP not set.
+echo %DATE% %TIME% - Error: Environment variable TEMP not set >>%UPDATE_LOGFILE%
+goto Error
+
+:NoTempDir
+echo ERROR: Directory "%TEMP%" not found.
+echo %DATE% %TIME% - Error: Directory "%TEMP%" not found >>%UPDATE_LOGFILE%
+goto Error
+
+:NoOSName
+echo ERROR: Environment variable OS_NAME not set.
+echo %DATE% %TIME% - Error: Environment variable OS_NAME not set >>%UPDATE_LOGFILE%
+goto Error
+
+:NoOSLang
+echo ERROR: Environment variable OS_LANG not set.
+echo %DATE% %TIME% - Error: Environment variable OS_LANG not set >>%UPDATE_LOGFILE%
+goto Error
+
+:NoOSArch
+echo ERROR: Environment variable OS_ARCH not set.
+echo %DATE% %TIME% - Error: Environment variable OS_ARCH not set >>%UPDATE_LOGFILE%
+goto Error
+
+:Error
+endlocal
+exit /b 1
+
+:EoF
+endlocal
---
+++ tags/wsusoffline7.2/client/cmd/PrepareRecall.cmd
@@ -0,0 +1,101 @@
+@echo off
+rem *** Author: T. Wittrock, Kiel ***
+
+if "%UPDATE_LOGFILE%"=="" set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
+
+if "%OS_NAME%"=="" goto NoOSName
+if "%REG_PATH%"=="" goto NoRegPath
+if "%CSCRIPT_PATH%"=="" goto NoCScriptPath
+
+if "%OS_NAME%"=="w60" goto SkipWinlogon
+if "%OS_NAME%"=="w61" goto SkipWinlogon
+echo Saving Winlogon registry hive...
+%REG_PATH% EXPORT "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" %SystemRoot%\woubak-winlogon.reg >nul 2>&1
+if errorlevel 1 (
+  echo ERROR: Saving of Winlogon registry hive failed.
+  echo %DATE% %TIME% - Error: Saving of Winlogon registry hive failed >>%UPDATE_LOGFILE%
+  goto Error
+) else (
+  echo %DATE% %TIME% - Info: Saved Winlogon registry hive >>%UPDATE_LOGFILE%
+)
+:SkipWinlogon
+
+echo Saving System policies registry hive...
+%REG_PATH% EXPORT "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" %SystemRoot%\woubak-system-policies.reg >nul 2>&1
+if errorlevel 1 (
+  echo %DATE% %TIME% - Info: Saving of System policies registry hive failed >>%UPDATE_LOGFILE%
+) else (
+  echo %DATE% %TIME% - Info: Saved System policies registry hive >>%UPDATE_LOGFILE%
+)
+
+if "%OS_NAME%"=="wxp" goto SkipPowerCfg
+if "%OS_NAME%"=="w2k3" goto SkipPowerCfg
+echo Creating temporary power scheme...
+for /F "delims=:( tokens=2" %%i in ('%SystemRoot%\system32\powercfg.exe -getactivescheme') do echo %%i>%SystemRoot%\woubak-pwrscheme-act.txt
+for /F %%i in (%SystemRoot%\woubak-pwrscheme-act.txt) do (
+  for /F "delims=:( tokens=2" %%j in ('%SystemRoot%\system32\powercfg.exe -duplicatescheme %%i') do echo %%j>%SystemRoot%\woubak-pwrscheme-temp.txt
+)
+for /F %%i in (%SystemRoot%\woubak-pwrscheme-temp.txt) do (
+  %SystemRoot%\system32\powercfg.exe -changename %%i WOUTemp
+  %SystemRoot%\system32\powercfg.exe -setactive %%i
+)
+if errorlevel 1 (
+  echo Warning: Activation of temporary power scheme failed.
+  echo %DATE% %TIME% - Warning: Activation of temporary power scheme failed >>%UPDATE_LOGFILE%
+) else (
+  echo %DATE% %TIME% - Info: Activated temporary power scheme >>%UPDATE_LOGFILE%
+)
+:SkipPowerCfg
+
+echo Preparing recall directory...
+if not exist %SystemRoot%\Temp\WOURecall\nul md %SystemRoot%\Temp\WOURecall
+for %%i in (CleanupRecall.cmd DeleteUpdateAdmin.vbs RecallStub.cmd Shutdown.vbs ..\bin\Autologon.exe) do copy /Y %%i %SystemRoot%\Temp\WOURecall >nul
+echo @%*>%SystemRoot%\Temp\WOURecall\RecallUpdate.cmd
+%SystemRoot%\system32\net.exe use %~d0 >nul 2>&1
+if errorlevel 1 (
+  echo %DATE% %TIME% - Info: WSUS Offline Update was started from a local drive ^(%~d0^) >>%UPDATE_LOGFILE%
+) else (
+  if exist %SystemRoot%\Temp\WOURecall\ReconnectNetDrive.cmd del %SystemRoot%\Temp\WOURecall\ReconnectNetDrive.cmd
+  for /F "tokens=1*" %%i in ('%SystemRoot%\system32\net.exe use %~d0') do echo @%SystemRoot%\system32\net.exe use %~d0 "%%j" | %SystemRoot%\system32\find.exe "\\">>%SystemRoot%\Temp\WOURecall\ReconnectNetDrive.cmd
+  echo %DATE% %TIME% - Info: WSUS Offline Update was started from a network drive ^(%~d0^) >>%UPDATE_LOGFILE%
+)
+echo %DATE% %TIME% - Info: Prepared recall directory >>%UPDATE_LOGFILE%
+
+echo Creating WOUTempAdmin account...
+%CSCRIPT_PATH% //Nologo //B //E:vbs CreateUpdateAdminAndEnableAutoLogon.vbs
+if errorlevel 1 (
+  echo Warning: Creation of WOUTempAdmin account failed.
+  echo %DATE% %TIME% - Warning: Creation of WOUTempAdmin account failed >>%UPDATE_LOGFILE%
+) else (
+  echo %DATE% %TIME% - Info: Created WOUTempAdmin account >>%UPDATE_LOGFILE%
+)
+
+echo Registering recall...
+%REG_PATH% ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v WSUSOfflineUpdate /t REG_SZ /d "%SystemRoot%\Temp\WOURecall\RecallStub.cmd" >nul 2>&1
+if errorlevel 1 (
+  echo Warning: Registration of recall failed.
+  echo %DATE% %TIME% - Warning: Registration of recall failed >>%UPDATE_LOGFILE%
+) else (
+  echo %DATE% %TIME% - Info: Registered recall >>%UPDATE_LOGFILE%
+)
+goto EoF
+
+:NoOSName
+echo ERROR: Environment variable OS_NAME not set.
+echo %DATE% %TIME% - Error: Environment variable OS_NAME not set >>%UPDATE_LOGFILE%
+goto Error
+
+:NoRegPath
+echo ERROR: Environment variable REG_PATH not set.
+echo %DATE% %TIME% - Error: Environment variable REG_PATH not set >>%UPDATE_LOGFILE%
+goto Error
+
+:NoCScriptPath
+echo ERROR: Environment variable CSCRIPT_PATH not set.
+echo %DATE% %TIME% - Error: Environment variable CSCRIPT_PATH not set >>%UPDATE_LOGFILE%
+goto Error
+
+:Error
+exit /b 1
+
+:EoF
