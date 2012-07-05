d2d67bfe04e096c3394e656962dae12751c1f90c
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2012-07-05 09:55:28 +0000

    - Prepare release
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@371 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -3,7 +3,7 @@
 ##########################################################
 ###           WSUS Offline Update Downloader           ###
 ###                  for Linux systems                 ###
-###                   v. 7.3.2+ (r370)                 ###
+###                       v. 7.4                       ###
 ###                                                    ###
 ###   http://www.wsusoffline.net/                      ###
 ###   Authors: Tobias Breitling, Stefan Joehnke,       ###
@@ -522,7 +522,7 @@ cat << END
 **********************************************************
 ***           WSUS Offline Update Downloader           ***
 ***                  for Linux systems                 ***
-***                   v. 7.3.2+ (r370)                 ***
+***                       v. 7.4                       ***
 ***                                                    ***
 ***   http://www.wsusoffline.net/                      ***
 ***   Authors: Tobias Breitling, Stefan Joehnke,       ***
