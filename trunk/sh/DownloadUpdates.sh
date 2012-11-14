30277a51c366cc9f4962ebdb3a026523b6cf4262
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2012-11-14 08:22:05 +0000

    - Browser choice update (kb976002) black listed for Windows 8 / Server 2012 downloads
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@411 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                           v. 7.5+ (r410)                          ###
+###                           v. 7.5+ (r411)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
