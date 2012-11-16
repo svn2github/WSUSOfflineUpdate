5448c9d4a1352fb224f64b90bb8fb0f61db1e039
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2012-11-16 11:13:09 +0000

    - Static definitions added for digital file signatures' update kb2769165 for Windows 8 / Server 2012 (x86/x64) (Thanks to "Gerby")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@413 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.0b (r412)                          ###
+###                           v. 8.0b (r413)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
