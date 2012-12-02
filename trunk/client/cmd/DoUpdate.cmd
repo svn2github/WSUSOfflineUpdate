5a5d9e4a9ae33bf80333f6589fae0f438b7e4f9b
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2012-12-02 21:05:30 +0000

    - Support added for C++ 2012 Runtime Libraries Update 1 (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@421 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/client/cmd/DoUpdate.cmd
+++ trunk/client/cmd/DoUpdate.cmd
@@ -9,7 +9,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=8.0b (r420)
+set WSUSOFFLINE_VERSION=8.0b (r421)
 title %~n0 %*
 echo Starting WSUS Offline Update (v. %WSUSOFFLINE_VERSION%) at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
