5cbf53aaa3c7bc72396c015364fee9f76f50d43b
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-01-22 08:05:39 +0000

    - Fix: Check for new WOU versions failed if environment variable PATH did not contain %SystemRoot%\system32 (Thanks to "surfmaxx" and "aker")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@437 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 8.0+ (r436)                           ###
+###                          v. 8.0+ (r437)                           ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
