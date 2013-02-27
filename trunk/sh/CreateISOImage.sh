7fd9f9026bfbc668d333e61666aaefc8ee202488
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-02-27 12:49:11 +0000

    - Fix: Invalid prerequisites lead to IE10 installation failure on Windows 7 and Server 2008 R2 (x86/x64) systems (Thanks to Th. Baisch)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@448 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                            v. 8.2b (r447)                         ###
+###                            v. 8.2b (r448)                         ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                           v. 8.2b (r447)                          ###
+###                           v. 8.2b (r448)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
