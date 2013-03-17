49dc87b6f8bf2b82e05e17f2d0408d1b028adc5a
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-03-17 16:10:29 +0000

    - Support removed for .NET Frameworks 2.0 SP0/SP1 and 3.5 SP0 (Thanks to M. Trenkner)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@457 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.2+ (r456)                          ###
+###                           v. 8.2+ (r457)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 8.2+ (r456)                           ###
+###                          v. 8.2+ (r457)                           ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
