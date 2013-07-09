0756f22647a193d699cd534e83d214e01f1c8ad5
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-07-09 20:35:07 +0000

    - Fix: Download part got updates for Lync (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@475 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.4+ (r474)                          ###
+###                           v. 8.4+ (r475)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 8.4+ (r474)                           ###
+###                          v. 8.4+ (r475)                           ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
