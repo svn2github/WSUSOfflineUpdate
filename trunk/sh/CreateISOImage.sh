a965be110ad465002f5e5f7e16ca1e6f44940521
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-07-09 17:06:40 +0000

    - Support added for C++ 2012 Runtime Libraries Update 3 (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@473 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.4+ (r472)                          ###
+###                           v. 8.4+ (r473)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 8.4+ (r472)                           ###
+###                          v. 8.4+ (r473)                           ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
