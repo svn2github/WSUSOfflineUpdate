3b18c2890258714908663955e68aff9ff439de7b
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-07-10 09:42:37 +0000

    - Fix: ExcludeList-ofc.txt contained trailing spaces (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@476 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.4+ (r475)                          ###
+###                           v. 8.4+ (r476)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 8.4+ (r475)                           ###
+###                          v. 8.4+ (r476)                           ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
