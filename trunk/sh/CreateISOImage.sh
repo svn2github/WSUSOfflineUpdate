43a8b080e5c90952280b4dde48ffbaf3b65ef5b0
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-07-29 07:58:34 +0000

    - Added missing URL for Turkish version of Service Pack 2 for Office 2010 x64 (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@483 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                               v. 8.5                              ###
+###                           v. 8.5+ (r483)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                               v. 8.5                              ###
+###                          v. 8.5+ (r483)                           ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
