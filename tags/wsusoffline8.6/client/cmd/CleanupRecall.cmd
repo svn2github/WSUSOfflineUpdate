f8068150ae122ebf1f6d552e019645a0e0086961
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-09-16 07:47:18 +0000

    - NOTE: If you like to use aria2 instead of wget for downloading, please call ActivateAria2Downloads.cmd once - Detection capability added for Windows 8.1 / Server 2012 R2 and Internet Explorer 11 - Optional use of aria2 instead of wget to speed up downloading (Thanks to "qoole" and "brb8two") - List of superseded updates will now be recalculated if ExcludeList-superseded-exclude.txt was modified - Microsoft Security Essentials and MSI packages will now be installed last (Thanks to "paul1149") - CreateISOImage.cmd and CreateISOImage.sh scripts will now create UDF instead of Joliet formatted ISO images (Thanks to "tapek") - DownloadUpdates.cmd script will now automatically update mkisofs tool - Added missing URL for Turkish version of Service Pack 2 for Office 2010 x64 (Thanks to "harry") - Fix: Static installation definitions for Office updates were not correctly evaluated on dual Office installations (Thanks to "aker") - Fix: XSLT filters for Windows 8 / Server 2012 also got updates for Windows 8.1 / Server 2012 R2 (Thanks to "harry") - Fix: UpdateGenerator script won't any longer try to copy to non-existent USB target paths (Thanks to "boco" and "aker") - Fix: DownloadUpdates.cmd script will now avoid misleading file integrity verification failures after an incomplete or broken download run (Thanks to "boco") - Fix: DownloadUpdates.cmd script erroneously deleted StaticDownloadLinks-o2k13-x64-glb.txt (Thanks to "psj") - Fix: Microsoft updated kb2574819 (DTLS, prerequisite for RDP 8.0) for Windows 7 and Server 2008 R2 (x86/x64) (Thanks to "cecinit" and "Matjes") - Fix: Download part missed kb2644615 for Windows 7 and Server 2008 R2 (x86/x64) due to a bug in Microsoft's update catalog file wsusscn2.cab - Fix: Download part missed kb956744 for Windows Server 2003 (x86) due to a bug in Microsoft's update catalog file wsusscn2.cab - Fix: Download part missed kb2817465 and kb2768017 for Office 2013 - Fix: Cleaned up ExcludeList-superseded-exclude.txt (Thanks to M. Trenkner) - Fix: Download failures of mkisofs tool will now result in a warning instead of an error (Thanks to Th. Lohe) - Fix: Minor fix in Linux script DownloadUpdates.sh (Thanks to "Buster" and H. Hullen)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@504 c6bda2d4-11f8-444b-8fa8-e24aff79bece

---
+++ tags/wsusoffline8.6/client/cmd/CleanupRecall.cmd
@@ -0,0 +1,120 @@
+@echo off
+rem *** Author: T. Wittrock, Kiel ***
+
+setlocal
+
+if "%UPDATE_LOGFILE%"=="" set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
+
+cd /D "%~dp0"
+
+if "%REG_PATH%"=="" set REG_PATH=%SystemRoot%\system32\reg.exe
+if not exist %REG_PATH% goto NoReg
+if "%CSCRIPT_PATH%"=="" set CSCRIPT_PATH=%SystemRoot%\system32\cscript.exe
+if not exist %CSCRIPT_PATH% goto NoCScript
+
+if exist %SystemRoot%\woubak-winlogon.reg (
+  echo Restoring Winlogon registry hive...
+  %REG_PATH% DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /va /f >nul 2>&1
+  %REG_PATH% IMPORT %SystemRoot%\woubak-winlogon.reg >nul 2>&1
+  del %SystemRoot%\woubak-winlogon.reg
+  echo %DATE% %TIME% - Info: Restored Winlogon registry hive >>%UPDATE_LOGFILE%
+)
+
+if exist %SystemRoot%\woubak-system-policies.reg (
+  echo Restoring System policies registry hive...
+  %REG_PATH% DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f >nul 2>&1
+  %REG_PATH% IMPORT %SystemRoot%\woubak-system-policies.reg >nul 2>&1
+  if errorlevel 1 (
+    echo Warning: Restore of System policies registry hive failed.
+    echo %DATE% %TIME% - Warning: Restore of System policies registry hive failed >>%UPDATE_LOGFILE%
+  ) else (
+    del %SystemRoot%\woubak-system-policies.reg
+    echo %DATE% %TIME% - Info: Restored System policies registry hive >>%UPDATE_LOGFILE%
+  )
+)
+
+if not exist %SystemRoot%\woubak-pwrscheme-act.txt goto SkipPowerCfg
+if not exist %SystemRoot%\woubak-pwrscheme-temp.txt goto SkipPowerCfg
+echo Deleting temporary power scheme...
+for /F %%i in (%SystemRoot%\woubak-pwrscheme-act.txt) do %SystemRoot%\system32\powercfg.exe -setactive %%i
+if errorlevel 1 (
+  echo Warning: Activation of previous power scheme failed.
+  echo %DATE% %TIME% - Warning: Activation of previous power scheme failed >>%UPDATE_LOGFILE%
+) else (
+  del %SystemRoot%\woubak-pwrscheme-act.txt
+  echo %DATE% %TIME% - Info: Activated previous power scheme >>%UPDATE_LOGFILE%
+)
+for /F %%i in (%SystemRoot%\woubak-pwrscheme-temp.txt) do %SystemRoot%\system32\powercfg.exe -delete %%i
+if errorlevel 1 (
+  echo Warning: Deletion of temporary power scheme failed.
+  echo %DATE% %TIME% - Warning: Deletion of temporary power scheme failed >>%UPDATE_LOGFILE%
+) else (
+  del %SystemRoot%\woubak-pwrscheme-temp.txt
+  echo %DATE% %TIME% - Info: Deleted temporary power scheme >>%UPDATE_LOGFILE%
+)
+:SkipPowerCfg
+
+echo Unregistering recall...
+%REG_PATH% DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v WSUSOfflineUpdate /f >nul 2>&1
+if errorlevel 1 (
+  echo Warning: Deregistration of recall failed.
+  echo %DATE% %TIME% - Warning: Deregistration of recall failed >>%UPDATE_LOGFILE%
+) else (
+  echo %DATE% %TIME% - Info: Unregistered recall >>%UPDATE_LOGFILE%
+)
+
+echo Disabling autologon...
+%REG_PATH% ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d "0" /f >nul 2>&1
+if errorlevel 1 (
+  echo Warning: Disabling of autologon failed.
+  echo %DATE% %TIME% - Warning: Disabling of autologon failed >>%UPDATE_LOGFILE%
+) else (
+  echo %DATE% %TIME% - Info: Disabled autologon >>%UPDATE_LOGFILE%
+)
+
+echo Deleting WOUTempAdmin account...
+if "%USERNAME%"=="WOUTempAdmin" (
+  if "%USERSID%"=="" (
+    echo %DATE% %TIME% - Warning: Environment variable USERSID not found - skipped deletion of registry reference to WOUTempAdmin profile >>%UPDATE_LOGFILE%
+  ) else (
+    %REG_PATH% DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\%USERSID%" /f >nul 2>&1
+    if errorlevel 1 (
+      echo %DATE% %TIME% - Warning: Registry reference to WOUTempAdmin profile not found - skipped deletion >>%UPDATE_LOGFILE%
+    ) else (
+      echo %DATE% %TIME% - Info: Deleted registry reference to WOUTempAdmin profile >>%UPDATE_LOGFILE%
+    )
+  )
+  %REG_PATH% ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v DeleteWOUTempAdminProfile /t REG_SZ /d "cmd /c rd /S /Q \"%USERPROFILE%\"" /f >nul 2>&1
+  echo %DATE% %TIME% - Info: Registered deletion of WOUTempAdmin profile >>%UPDATE_LOGFILE%
+  %REG_PATH% ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v DeleteWOURecallDir /t REG_SZ /d "cmd /c rd /S /Q \"%SystemRoot%\Temp\WOURecall\"" /f >nul 2>&1
+  echo %DATE% %TIME% - Info: Registered deletion of recall directory >>%UPDATE_LOGFILE%
+) else (
+  echo %DATE% %TIME% - Warning: WOUTempAdmin is not logged on - skipped registration of profile and recall directory deletion >>%UPDATE_LOGFILE%
+)
+%CSCRIPT_PATH% //Nologo //B //E:vbs DeleteUpdateAdmin.vbs
+if errorlevel 1 (
+  echo Warning: Deletion of WOUTempAdmin account failed.
+  echo %DATE% %TIME% - Warning: Deletion of WOUTempAdmin account failed >>%UPDATE_LOGFILE%
+) else (
+  echo %DATE% %TIME% - Info: Deleted WOUTempAdmin account >>%UPDATE_LOGFILE%
+)
+goto EoF
+
+:NoReg
+echo.
+echo ERROR: Registry tool %REG_PATH% not found.
+echo %DATE% %TIME% - Error: Registry tool %REG_PATH% not found >>%UPDATE_LOGFILE%
+goto Error
+
+:NoCScript
+echo.
+echo ERROR: VBScript interpreter %CSCRIPT_PATH% not found.
+echo %DATE% %TIME% - Error: VBScript interpreter %CSCRIPT_PATH% not found >>%UPDATE_LOGFILE%
+goto Error
+
+:Error
+endlocal
+exit /b 1
+
+:EoF
+endlocal
---
+++ tags/wsusoffline8.6/cmd/ActivateAria2Downloads.cmd
@@ -0,0 +1,115 @@
+@echo off
+rem *** Author: T. Wittrock, Kiel ***
+
+verify other 2>nul
+setlocal enableextensions
+if errorlevel 1 goto NoExtensions
+
+if "%DIRCMD%" NEQ "" set DIRCMD=
+
+cd /D "%~dp0"
+
+set DOWNLOAD_LOGFILE=..\log\download.log
+if exist %DOWNLOAD_LOGFILE% (
+  echo. >>%DOWNLOAD_LOGFILE%
+  echo -------------------------------------------------------------------------------- >>%DOWNLOAD_LOGFILE%
+  echo. >>%DOWNLOAD_LOGFILE%
+)
+echo %DATE% %TIME% - Info: Activating Aria2 downloads >>%DOWNLOAD_LOGFILE%
+
+set WGET_PATH=..\bin\wget.exe
+if not exist %WGET_PATH% goto NoWGet
+if not exist ..\bin\unzip.exe goto NoUnZip
+
+:EvalParams
+if "%1"=="" goto NoMoreParams
+if /i "%1"=="/proxy" (
+  set http_proxy=%2
+  shift /1
+)
+shift /1
+goto EvalParams
+
+:NoMoreParams
+rem *** Activate Aria2 downloads ***
+title Activating Aria2 downloads...
+if exist ..\bin\aria2c-x64.exe goto DLx86
+if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto DLx64
+if /i "%PROCESSOR_ARCHITEW6432%"=="AMD64" goto DLx64
+goto DLx86
+:DLx64
+echo Downloading most recent version of Aria2 (x64)...
+for /F %%i in ('%SystemRoot%\system32\findstr.exe /I "64bit" ..\static\StaticDownloadLinks-aria2.txt') do (
+  %WGET_PATH% -N -P ..\bin %%i
+  if errorlevel 1 goto DownloadError
+  echo %DATE% %TIME% - Info: Downloaded most recent version of Aria2 ^(x64^)>>%DOWNLOAD_LOGFILE%
+  echo Unpacking aria2c.exe from ..\bin\%%~nxi to ..\bin\aria2c-x64.exe...
+  ..\bin\unzip.exe -p ..\bin\%%~nxi */aria2c.exe >..\bin\aria2c-x64.exe
+  echo %DATE% %TIME% - Info: Unpacked aria2c.exe ..\bin\%%~nxi to ..\bin\aria2c-x64.exe >>%DOWNLOAD_LOGFILE%
+  del ..\bin\%%~nxi
+  echo %DATE% %TIME% - Info: Deleted %%~nxi >>%DOWNLOAD_LOGFILE%
+)
+:DLx86
+if exist ..\bin\aria2c-x86.exe goto Activate
+echo Downloading most recent version of Aria2 (x86)...
+for /F %%i in ('%SystemRoot%\system32\findstr.exe /I "32bit" ..\static\StaticDownloadLinks-aria2.txt') do (
+  %WGET_PATH% -N -P ..\bin %%i
+  if errorlevel 1 goto DownloadError
+  echo %DATE% %TIME% - Info: Downloaded most recent version of Aria2 ^(x86^)>>%DOWNLOAD_LOGFILE%
+  echo Unpacking aria2c.exe from ..\bin\%%~nxi to ..\bin\aria2c-x86.exe...
+  ..\bin\unzip.exe -p ..\bin\%%~nxi */aria2c.exe >..\bin\aria2c-x86.exe
+  echo %DATE% %TIME% - Info: Unpacked aria2c.exe ..\bin\%%~nxi to ..\bin\aria2c-x86.exe >>%DOWNLOAD_LOGFILE%
+  del ..\bin\%%~nxi
+  echo %DATE% %TIME% - Info: Deleted %%~nxi >>%DOWNLOAD_LOGFILE%
+)
+:Activate
+echo if /i "%%PROCESSOR_ARCHITECTURE%%"=="AMD64" goto x64>custom\SetAria2EnvVars.cmd
+echo if /i "%%PROCESSOR_ARCHITEW6432%%"=="AMD64" goto x64>>custom\SetAria2EnvVars.cmd
+echo goto x86>>custom\SetAria2EnvVars.cmd
+echo.>>custom\SetAria2EnvVars.cmd
+echo :x64>>custom\SetAria2EnvVars.cmd
+echo if exist ..\bin\aria2c-x64.exe (set DLDR_PATH=..\bin\aria2c-x64.exe) else (goto x86)>>custom\SetAria2EnvVars.cmd 
+echo goto SetParams>>custom\SetAria2EnvVars.cmd
+echo.>>custom\SetAria2EnvVars.cmd
+echo :x86>>custom\SetAria2EnvVars.cmd
+echo set DLDR_PATH=..\bin\aria2c-x86.exe>>custom\SetAria2EnvVars.cmd 
+echo.>>custom\SetAria2EnvVars.cmd
+echo :SetParams>>custom\SetAria2EnvVars.cmd
+echo set DLDR_COPT=--conditional-get=true --allow-overwrite=true --file-allocation=none -x10 -j10 -s10 -k1M -R>>custom\SetAria2EnvVars.cmd
+echo set DLDR_LOPT=--log-level=notice -l %%DOWNLOAD_LOGFILE%%>>custom\SetAria2EnvVars.cmd
+echo set DLDR_IOPT=-i>>custom\SetAria2EnvVars.cmd 
+echo set DLDR_POPT=-d>>custom\SetAria2EnvVars.cmd 
+echo set DLDR_NVOPT=--console-log-level=warn>>custom\SetAria2EnvVars.cmd 
+goto EoF
+
+:NoExtensions
+echo.
+echo ERROR: No command extensions available.
+echo.
+exit
+
+:NoWGet
+echo.
+echo ERROR: Download utility %WGET_PATH% not found.
+echo %DATE% %TIME% - Error: Download utility %WGET_PATH% not found >>%DOWNLOAD_LOGFILE%
+echo.
+goto EoF
+
+:NoUnZip
+echo.
+echo ERROR: Utility ..\bin\unzip.exe not found.
+echo %DATE% %TIME% - Error: Utility ..\bin\unzip.exe not found >>%DOWNLOAD_LOGFILE%
+echo.
+goto EoF
+
+:DownloadError
+echo.
+echo ERROR: Download of most recent version of Aria2 failed.
+echo %DATE% %TIME% - Error: Download of most recent version of Aria2 failed >>%DOWNLOAD_LOGFILE%
+echo.
+goto EoF
+
+:EoF
+echo %DATE% %TIME% - Info: Activated Aria2 downloads >>%DOWNLOAD_LOGFILE%
+title %ComSpec%
+endlocal
