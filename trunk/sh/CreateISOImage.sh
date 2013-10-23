83b4d74647bcb3cb84f438a6ee72ff072c026b88
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-10-23 13:31:20 +0000

    - Update for the Windows Operating System Loader (kb2506014) added for Windows Vista, 7 and Server 2008(R2) (x64) (Thanks to "friday123") - Static download definitions added for Update for Deprecation of MD5 Hashing Algorithm for Microsoft Root Certificate Program (kb2862973) for Windows Vista, 7, 8 and Server 2008(R2)/2012 (x86/x64) (Thanks to "friday123") - Excluded Update Rollups 1-4 (kb2554636, kb2630436, kb2705122 and kb2757013) for Windows Storage Server 2008 R2 Essentials (Thanks to "zhaojie")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@514 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.6+ (r513)                          ###
+###                           v. 8.6+ (r514)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
