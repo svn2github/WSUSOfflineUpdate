90b64acd2f723b442f75987593ea33dc1afe8da0
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-11-13 16:43:44 +0000

    - Fix: Office multilingual updates' directory (ofc glb) was blown up (Thanks to M. Köhler and "Denniss") - Fix: Download part missed Security Updates kb2760494 for Office 2003 - Fix: Download part missed Security Updates kb2760415 for Office 2007 - Fix: Download part missed Security Updates kb2553284 and kb2760781 for Office 2010 - Fix: Download part missed Security Updates kb2768005 and kb2837618 for Office 2013
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@530 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.8b (r529)                          ###
+###                           v. 8.8b (r530)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
