42d1d98ae5f43f14fc0fbc22f7d7fb5556dea1c6
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2013-12-11 11:15:56 +0000

    - Replaced Windows XP/Vista scan prerequisite update kb2888505 by kb2898785
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@541 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.8+ (r540)                          ###
+###                           v. 8.8+ (r541)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 8.8+ (r540)                           ###
+###                          v. 8.8+ (r541)                           ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
