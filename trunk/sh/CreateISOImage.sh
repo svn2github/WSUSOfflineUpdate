351906397673c1be2e424cc0e9b4237f60e312fb
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2014-01-21 12:20:46 +0000

    - Fix: CompileAutoItScripts.cmd script won't any longer use compression for compilation in order to avoid false positive alerts from AV scanners (Thanks to "boco")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@555 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 8.9+ (r554)                          ###
+###                           v. 8.9+ (r555)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
