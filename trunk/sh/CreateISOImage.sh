1afed5cb49e994afa08e23eaff50205e1e5dcf9f
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2014-03-12 19:38:19 +0000

    - Microsoft Silverlight updated to v. 5.1.30214.0 (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@566 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 9.0+ (r565)                          ###
+###                           v. 9.0+ (r566)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
