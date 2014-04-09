ccd107167136e3c16159e680048b09a24a985a84
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2014-04-09 08:06:41 +0000

    - Fix: Excluded download of kb2927581 (Update Rollup 1.2 for Windows MultiPoint Server 2011 on Windows Server 2008 R2 systems)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@571 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                               v. 9.1                              ###
+###                           v. 9.1+ (r571)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
