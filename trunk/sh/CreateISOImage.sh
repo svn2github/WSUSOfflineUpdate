8681358a085c7421a6d009c478a161d55881af80
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2014-05-16 11:04:46 +0000

    - Fix: Microsoft corrected Windows 8.1 Update 1 (kb2919355) again by adding clearcompressionflag.exe
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@583 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 9.2.1+ (r582)                        ###
+###                           v. 9.3b (r583)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
