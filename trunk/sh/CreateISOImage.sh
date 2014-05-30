e6cdfa260d0c620dd2f664aed8b6ad0dae668de6
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2014-05-30 07:10:50 +0000

    - Fix: Root Certificates' updates kb931125 and kb2917500 added to installation exclude list (explicit installation) (Thanks to "aker")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@592 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 9.3+ (r591)                          ###
+###                           v. 9.3+ (r592)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
