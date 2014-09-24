aaf519d8a37ceb464e35457ab166b3b849876e9f
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2014-09-24 17:52:37 +0000

    - Fix: Download part did not exclude kb2976627 (superseded cumulative security update for IE) for Windows Vista and Server 2008 (x86/x64) (Thanks to "Matjes")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@618 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 9.4+ (r617)                          ###
+###                           v. 9.4+ (r618)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
