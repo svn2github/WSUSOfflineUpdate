4d885d126dbbb8fc4dc5d9111bd9574ee0318590
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2015-02-03 17:27:28 +0000

    - Static download URLs for aria2 download utility updated to v. 1.18.9 (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@641 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                               v. 9.5.1                            ###
+###                           v. 9.5.1+ (r641)                        ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                              v. 9.5.1                             ###
+###                          v. 9.5.1+ (r641)                         ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
