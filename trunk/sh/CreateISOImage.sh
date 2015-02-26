25e8e2517a0fc95be07aa332b2b50289b307fb9a
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2015-02-26 17:27:56 +0000

    - Fix: Download part missed kb2535512 (MS11-042) for Windows Vista and Server 2008 (x86/x64) systems - Fix: Download part missed kb2536276 (MS11-043) for Windows Vista/7 and Server 2008(R2) (x86/x64) systems
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@648 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 9.5.2+ (r647)                        ###
+###                           v. 9.5.2+ (r648)                        ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
