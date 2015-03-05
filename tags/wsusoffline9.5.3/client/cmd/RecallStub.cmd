4009d8d7f468a1b5c5af7025564bda83ca0622bb
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2015-03-05 13:26:55 +0000

    - Update for Root Certificates (rootsupd.exe/kb931125) updated to v. 41,0,2195,0 (March 2014) (Thanks to "Gerby", "boco", "-Iwan-" and "aker") - Added fix for text quality degradation (kb3037639) after security update kb3013455 for Windows Vista and Server 2003/2008 (x86/x64) systems - Increased filtering stability by operating findstr.exe case insensitively (Thanks to "hbuhrmester") - Self update will now automatically update custom language additions (Thanks to "aker") - Static download URLs for aria2 download utility updated to v. 1.18.10 (Thanks to "harry") - Fix: Replaced obsolete Security Update for Office 2010 kb2878284 (MS14-023) by kb2956073 (MS15-012)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@652 c6bda2d4-11f8-444b-8fa8-e24aff79bece

---
+++ tags/wsusoffline9.5.3/client/cmd/RecallStub.cmd
@@ -0,0 +1,44 @@
+@echo off
+rem *** Author: T. Wittrock, Kiel ***
+
+setlocal
+
+if "%UPDATE_LOGFILE%"=="" set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
+
+cd /D "%~dp0"
+
+if exist %UPDATE_LOGFILE% echo.>>%UPDATE_LOGFILE%
+if not exist RecallUpdate.cmd (
+  echo %DATE% %TIME% - Error: Script file RecallUpdate.cmd not found>>%UPDATE_LOGFILE%
+  goto Cleanup
+)
+if not exist ReconnectNetDrive.cmd goto SkipReconnect
+%SystemRoot%\System32\timeout.exe /T 2 >nul
+for /F "tokens=3*" %%i in (ReconnectNetDrive.cmd) do (
+  echo Reconnecting network drive %%i to %%j...
+  call ReconnectNetDrive.cmd
+  if errorlevel 1 (
+    echo %DATE% %TIME% - Error: Reconnection of network drive %%i to %%j failed>>%UPDATE_LOGFILE%
+    goto Cleanup
+  )
+  echo %DATE% %TIME% - Info: Reconnected network drive %%i to %%j>>%UPDATE_LOGFILE%
+)
+:SkipReconnect
+echo Recalling update...
+RecallUpdate.cmd
+goto EoF
+
+:Cleanup
+echo Cleaning up automatic recall...
+call CleanupRecall.cmd
+if exist %SystemRoot%\System32\bcdedit.exe (
+  echo Adjusting boot sequence for next reboot...
+  %SystemRoot%\System32\bcdedit.exe /bootsequence {current}
+  echo %DATE% %TIME% - Info: Adjusted boot sequence for next reboot>>%UPDATE_LOGFILE%
+)
+echo Rebooting...
+%SystemRoot%\System32\shutdown.exe /r /f /t 1
+goto EoF
+
+:EoF
+endlocal
