03c3c6dc441c1c6b5ca41b3fb420cd95abfa295a
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2015-03-28 18:23:48 +0000

    - Update for untrusted/revoked Root Certificates (rvkroots.exe) updated to v. 8,0,2195,0 (kb3050995, March 2015) (Thanks to "harry" and "aker")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@658 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 9.6b (r657)                           ###
+###                          v. 9.6b (r658)                           ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
