d6188186a14bfd028e5f79b2ca9fc2e195d6c9c8
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2015-10-14 14:23:20 +0000

    - Fix: Removed obsolete September 2015 cumulative update for Windows 10 (kb3081455) and prerequisite compatibility update (kb3081452) - Fix: Replaced obsolete Visio 2010 Security Update kb3054876 (MS15-081) by kb3085514 (MS15-110)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@702 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 10.1+ (r701)                          ###
+###                          v. 10.1+ (r702)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
