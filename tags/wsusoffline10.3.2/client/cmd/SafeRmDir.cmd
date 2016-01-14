f83c5e8cbd618981c6c7f2bcf6e266b1509e8907
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-01-14 15:49:32 +0000

    - Microsoft Silverlight updated to v. 5.1.41212.0 (Thanks to "harry") - Fix: Replaced superseded Visio 2010 Security Update kb3101526 (MS15-116) by kb3114402 (MS16-004) - Fix: Replaced superseded Visio 2016 Security Update kb3101507 (MS15-116) by kb3114511 (MS16-004) - Fix: Moved kb2552343 for Windows 7 and Server 2008 R2 (x86/x64) from static installation definitions to scan prerequisites (Thanks to "jonas" and "aker") - Fix: Removed kb2592687 from Remote Desktop Connection 8.1 client installation on Windows 7 / Server 2008 R2 (x86/x64) systems (Thanks to "jonas") - Fix: Excluded obsolete kb3097877 for Windows 10 x64 from download (Thanks to "aker") - Fix: AddCustomLanguageSupport.cmd script contained invalid file name prefix for .NET Framework 4.6.1 (Thanks to "hbuhrmester")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@717 c6bda2d4-11f8-444b-8fa8-e24aff79bece

---
+++ tags/wsusoffline10.3.2/client/cmd/SafeRmDir.cmd
@@ -0,0 +1,46 @@
+@echo off
+rem *** Author: T. Wittrock, Kiel ***
+
+if "%UPDATE_LOGFILE%"=="" set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
+
+if '%1'=='' goto NoParam
+if not exist %1 goto EoF
+
+if "%CSCRIPT_PATH%"=="" (
+  if exist %SystemRoot%\Sysnative\cscript.exe (
+    set CSCRIPT_PATH=%SystemRoot%\Sysnative\cscript.exe
+  ) else (
+    set CSCRIPT_PATH=%SystemRoot%\System32\cscript.exe
+  )
+)
+if not exist %CSCRIPT_PATH% goto NoCScript
+
+pushd "%~dp0"
+for /L %%i in (1,1,10) do (
+  if exist %1 rd /S /Q %1 >nul 2>&1
+  if exist %1 %CSCRIPT_PATH% //Nologo //B //E:vbs Sleep.vbs 100
+)
+popd
+if exist %1 goto RmError
+goto EoF
+
+:NoParam
+echo ERROR: Invalid parameter. Usage: %~n0 ^<directory^>
+echo %DATE% %TIME% - Error: Invalid parameter. Usage: %~n0 ^<directory^>>>%UPDATE_LOGFILE%
+goto Error
+
+:NoCScript
+echo ERROR: VBScript interpreter %CSCRIPT_PATH% not found.
+echo %DATE% %TIME% - Error: VBScript interpreter %CSCRIPT_PATH% not found>>%UPDATE_LOGFILE%
+goto Error
+
+:RmError
+echo Warning: Removal of temporary directory %1 failed.
+echo %DATE% %TIME% - Warning: Removal of temporary directory %1 failed>>%UPDATE_LOGFILE%
+goto Error
+
+:Error
+exit /b 1
+
+:EoF
+exit /b 0
