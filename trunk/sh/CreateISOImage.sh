170102997703519538dbf6a5e3f38944a61f1c1d
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-02-03 19:45:44 +0000

    - Replaced superseded Windows Update Client December 2015 (kb3112343/kb3112336) for Windows 7 / 8.1 (x86/x64) and Server 2008/2012 R2 by Windows Update Client February 2016 (kb3135445/kb3135449) (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@731 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                               v. 10.4                             ###
+###                           v. 10.4+ (r731)                         ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                              v. 10.4                              ###
+###                          v. 10.4+ (r731)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
