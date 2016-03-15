5ef62e5e441cee9fbe76ae75c7dbe09ea93b6b69
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-03-15 07:08:08 +0000

    - Fix: Typo (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@754 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.6+ (r753)                         ###
+###                           v. 10.6+ (r754)                         ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
