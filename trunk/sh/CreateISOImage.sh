817728d90d84747c0c52450bc9dde670bcc6c4eb
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-04-15 14:11:54 +0000

    - C++ 2015 Runtime Libraries updated to v. 14.0.23918 (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@762 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.6.1+ (r761)                       ###
+###                           v. 10.6.1+ (r762)                       ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
