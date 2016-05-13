8680348f961f6fab336dbdab6e631c10be13d033
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-05-13 10:35:33 +0000

    - Added scan prerequisite update kb3153199 (MS16-062) (Thanks to "jonas", "Denniss" and "Dalai")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@770 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.6.2+ (r769)                       ###
+###                           v. 10.6.2+ (r770)                       ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 10.6.2+ (r769)                        ###
+###                          v. 10.6.2+ (r770)                        ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
