5489b8715c8068ddbf7023bca37e4a4f4b7f7c9d
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-06-15 09:44:12 +0000

    - Replaced superseded scan prerequisite update kb3153199 (MS16-062) by kb3161664 (MS16-073) (Thanks to "Denniss" and "Dalai")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@772 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.6.2+ (r771)                       ###
+###                           v. 10.6.2+ (r772)                       ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 10.6.2+ (r771)                        ###
+###                          v. 10.6.2+ (r772)                        ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
