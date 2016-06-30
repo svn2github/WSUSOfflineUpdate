1bc4f2f0b9939667ef02b4b7e96dd4de01713aca
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-06-30 14:09:29 +0000

    - Microsoft Silverlight updated to v. 5.1.50428.00 (Thanks to "mAsTa")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@777 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/client/UpdateInstaller.au3
+++ trunk/client/UpdateInstaller.au3
@@ -6,7 +6,7 @@
 #RequireAdmin
 #pragma compile(CompanyName, "T. Wittrock")
 #pragma compile(FileDescription, "WSUS Offline Update Installer")
-#pragma compile(FileVersion, 10.6.3.776)
+#pragma compile(FileVersion, 10.6.3.777)
 #pragma compile(InternalName, "Installer")
 #pragma compile(LegalCopyright, "GNU GPLv3")
 #pragma compile(OriginalFilename, UpdateInstaller.exe)
--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.6.3+ (r776)                       ###
+###                           v. 10.6.3+ (r777)                       ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
