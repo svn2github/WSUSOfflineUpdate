0284ae8debd3220ec27924a5b9624afc3dc46332
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-07-14 15:06:11 +0000

    - Fix: Duplicates in Office exclude lists (Thanks to "Denniss")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@787 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                              v. 10.7b (r786)                       ###
+###                              v. 10.7b (r787)                       ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                             v. 10.7b (r786)                        ###
+###                             v. 10.7b (r787)                        ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
