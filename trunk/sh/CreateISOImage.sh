aeff3bff07b89dd198157df1749cf24c7dad2331
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-09-19 12:57:14 +0000

    - Replaced superseded scan prerequisite update kb3177725 (MS16-098) by kb3185911 (MS16-106) for Windows Vista and Server 2008 (x86/x64) (Thanks to "Dalai") - C++ 2015 Redistributable Runtime Libraries updated to v. 14.0.24215.1 (Thanks to "Denniss") - Aria2 download utility updated to v. 1.27.1 (Thanks to "harry")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@817 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.8b (r816)                         ###
+###                           v. 10.8b (r817)                         ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 10.8b (r816)                          ###
+###                          v. 10.8b (r817)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
