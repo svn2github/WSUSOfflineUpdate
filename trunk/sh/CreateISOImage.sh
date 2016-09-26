f85fc54a35ac492c25b5d63285a0860ccbe50c4c
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-09-26 10:40:17 +0000

    - Replaced superseded scan prerequisite servicing stack update kb3020369 by kb3177467 for Windows 7 (x86/x64) and Server 2008 R2 (Thanks to "aker") - Replaced superseded July 2016 Servicing stack update (kb3173428) by September 2016 Servicing stack update (kb3181403) for Windows 10 Version 1511 - Added static download and installation definitions for August 2016 Servicing stack update for Windows 10 Version 1607 (kb3176936)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@822 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.8b (r821)                         ###
+###                           v. 10.8b (r822)                         ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 10.8b (r821)                          ###
+###                          v. 10.8b (r822)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
