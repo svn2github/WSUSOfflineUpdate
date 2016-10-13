0432db1b3b0dc00c250a62c6ff5e9dfc2e2d4f74
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-10-13 15:37:21 +0000

    - Fix: Added scan prerequisite update kb3183431 (MS16-123) for Windows Vista and Server 2008 (x86/x64) (Thanks to "Dalai") - Fix: ISO and USB images for Windows Vista and Server 2008 (x86/x64) contained unused installation packages for .NET Framework 4.6.2
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@827 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/client/cmd/DoUpdate.cmd
+++ trunk/client/cmd/DoUpdate.cmd
@@ -9,7 +9,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=10.8+ (r826)
+set WSUSOFFLINE_VERSION=10.8+ (r827)
 title %~n0 %*
 echo Starting WSUS Offline Update (v. %WSUSOFFLINE_VERSION%) at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
