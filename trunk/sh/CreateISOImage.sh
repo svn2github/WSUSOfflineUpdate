d94d96b522963e891b4f2a7d2db3a693dd8298f3
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2016-11-15 16:16:16 +0000

    - Reviewed ExcludeList-superseded-exclude.txt
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@832 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.8+ (r831)                         ###
+###                           v. 10.8+ (r832)                         ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
--- trunk/sh/DownloadUpdates.sh
+++ trunk/sh/DownloadUpdates.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###         WSUS Offline Update Downloader for Linux systems          ###
-###                          v. 10.8+ (r831)                          ###
+###                          v. 10.8+ (r832)                          ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Tobias Breitling, Stefan Joehnke, Walter Schiessberg   ###
