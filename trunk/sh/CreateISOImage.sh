23c965a2a6bd3e695c9d9758d94c8a1efa418b0b
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2017-01-17 14:49:34 +0000

    - Aria2 download utility updated to v. 1.31.0 (Thanks to "harry" and "Rush") - January 2017 updates added to 'security only' lists for Windows 7 and Server 2012 R2 (x86/x64) systems (Thanks to "hbuhrmester" and "boco")
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@851 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/sh/CreateISOImage.sh
+++ trunk/sh/CreateISOImage.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 ###          WSUS Offline Update ISO maker for Linux systems          ###
-###                           v. 10.9                                 ###
+###                        v. 10.9+ (r851)                            ###
 ###                                                                   ###
 ###   http://www.wsusoffline.net/                                     ###
 ###   Authors: Stefan Joehnke, Walter Schiessberg                     ###
