604a8a47633d75f02d80482d5d47afd412421468
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2017-10-07 14:44:13 +0000

    - Prepare release
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@899 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/client/cmd/DoUpdate.cmd
+++ trunk/client/cmd/DoUpdate.cmd
@@ -9,7 +9,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=11.0.1+ (r898)
+set WSUSOFFLINE_VERSION=11.0.2
 title %~n0 %*
 echo Starting WSUS Offline Update (v. %WSUSOFFLINE_VERSION%) at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
