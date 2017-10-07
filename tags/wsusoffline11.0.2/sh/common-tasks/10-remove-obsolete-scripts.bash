51ca0c4965357cc87fa7d8757bc01db6a38a8709
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2017-10-07 14:44:50 +0000

    - NOTE: This version will be the last one supporting Office 2007 (see https://support.microsoft.com/en-us/help/3198497/office-2007-approaching-end-of-extended-support) - August 2017 updates added to 'security only' lists for Windows 7 / 8.1 and Server 2008 R2 / 2012 / 2012 R2 (x86/x64) systems (Thanks to "hbuhrmester" and "Dalai") - September 2017 updates added to 'security only' lists for Windows 7 / 8.1 and Server 2008 R2 / 2012 / 2012 R2 (x86/x64) systems - Included complete rewrite of the Linux scripts version 1.0-beta-5 (Special thanks to H. Buhrmester) - C++ 2017 Redistributable Runtime Libraries updated to v. 14.11.25325.0 (Thanks to "harry") - UpdateGenerator script will now use default text editor for displaying download log file (Thanks to "bpit") - DoUpdate.cmd script will now signal errors by setting environment variable ERROR_OCCURRED (Thanks to "sklaes") - Fix: Determination of 'Windows Update' service state could fail due to language dependent output of service control utility sc.exe (Thanks to "georgeman", "Dalai" and "aker") - Fix: Added missing Outlook 2007 Security Update kb4011110 (September 2017) - Fix: Added missing Outlook 2016 Security Update kb4011052 (July 2017) (Thanks to "arturk77") - Fix: Replaced superseded Office 2007 Security Update kb3118304 (June 2017) by kb3213646 (September 2017) - Fix: Replaced superseded Outlook 2010 Security Update kb2956078 (July 2017) by kb4011089 (September 2017) - Fix: Replaced superseded Office 2010 Security Update kb3203461 (June 2017) by kb3213626 (September 2017) - Fix: Replaced superseded Office 2013 Security Update kb3203392 (June 2017) by kb3213564 (September 2017) - Fix: Replaced superseded Skype for Business 2015 Security Update kb3191937 (June 2017) by kb3213568 (September 2017) - Fix: Replaced superseded Skype for Business 2015 Security Update kb3191939 (June 2017) by kb4011107 (September 2017) - Fix: Replaced superseded Office 2016 Security Update kb3203383 (June 2017) by kb3213551 (September 2017) - Fix: Replaced superseded Skype for Business 2016 Security Update kb3203382 (June 2017) by kb4011040 (September 2017)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@900 c6bda2d4-11f8-444b-8fa8-e24aff79bece

---
+++ tags/wsusoffline11.0.2/sh/common-tasks/10-remove-obsolete-scripts.bash
@@ -0,0 +1,73 @@
+# This file will be sourced by the shell bash.
+#
+# Filename: 10-remove-obsolete-scripts.bash
+# Version: 1.0-beta-5
+# Release date: 2017-08-25
+# Intended compatibility: WSUS Offline Update Version 11.0.1 and newer
+#
+# Copyright (C) 2016-2017 Hartmut Buhrmester
+#                         <zo3xaiD8-eiK1iawa@t-online.de>
+#
+# License
+#
+#     This file is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published
+#     by the Free Software Foundation, either version 3 of the License,
+#     or (at your option) any later version.
+#
+#     This file is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     General Public License for more details.
+#
+#     You should have received a copy of the GNU General
+#     Public License along with this program.  If not, see
+#     <http://www.gnu.org/licenses/>.
+#
+# Description
+#
+#     During the development of the new Linux scripts, tasks and
+#     libraries sometimes need to be replaced or renumbered. Then
+#     the first task would be to delete obsolete files of previous
+#     versions. Therefore, this new task is inserted as the first file
+#     in the directory common-tasks.
+
+# ========== Functions ====================================================
+
+function remove_obsolete_scripts ()
+{
+    # Remove the obsolete script DownloadUpdates.sh and related files.
+    #
+    # The new Linux scripts are included in the main WSUS Offline Update
+    # archive in changeset 866 http://trac.wsusoffline.net/changeset/866 ,
+    # but then the old scripts in the same directory need to be removed.
+    rm -f ./commonparts.inc
+    rm -f ./CreateISOImage.sh
+    rm -f ./DownloadUpdates.sh
+    rm -f ./RemoveGermanAndEnglishLanguageSupport.sh
+
+    # Remove obsolete scripts from version 1.0-beta-2
+    #
+    # The directory common-tasks was refactored in version 1.0-beta-3:
+    # A new script 10-remove-obsolete-scripts.bash (this one) was added,
+    # and the script 40-check-for-self-updates.bash was split into two
+    # smaller scripts. The other scripts were renumbered. Thus, all
+    # scripts from version 1.0-beta-2 in this directory, if present,
+    # need to be removed.
+    rm -f ./common-tasks/10-start-logging.bash
+    rm -f ./common-tasks/20-check-needed-applications.bash
+    rm -f ./common-tasks/30-configure-downloaders.bash
+    rm -f ./common-tasks/40-check-for-self-updates.bash
+
+    # Remove old documentation files from version 1.0-beta-3
+    #
+    # These files were renamed in version 1.0-beta-4 to
+    # Installation_Guide.txt and Installationsanleitung.txt.
+    rm -f ./documentation/Quick_installation_guide.txt
+    rm -f ./documentation/Kurzinstallationsanleitung.txt
+}
+
+# ========== Commands =====================================================
+
+remove_obsolete_scripts
+return 0
