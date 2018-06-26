901538bd4199a1b40c40447e72820a5697dadc11
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2018-06-26 14:35:37 +0000

    - Fix: Download part missed Security Updates kb2973112 and kb3122648 for .NET Framework 3.5.1 on Windows 7 / Server 2008 R2 (x86/x64) systems
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@973 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/client/cmd/DoUpdate.cmd
+++ trunk/client/cmd/DoUpdate.cmd
@@ -9,7 +9,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=11.4+ (r972)
+set WSUSOFFLINE_VERSION=11.4+ (r973)
 title %~n0 %*
 echo Starting WSUS Offline Update (v. %WSUSOFFLINE_VERSION%) at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
