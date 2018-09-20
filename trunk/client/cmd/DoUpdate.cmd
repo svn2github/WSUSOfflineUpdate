b33f4905004d71739466d14129b60d66af264705
twittrock <twittrock@c6bda2d4-11f8-444b-8fa8-e24aff79bece>
2018-09-20 14:52:33 +0000

    - Fix: ISO and USB filters corrected (Thanks to H. Buhrmester)
    git-svn-id: https://svn.wsusoffline.net/svn/wsusoffline@989 c6bda2d4-11f8-444b-8fa8-e24aff79bece

--- trunk/client/cmd/DoUpdate.cmd
+++ trunk/client/cmd/DoUpdate.cmd
@@ -9,7 +9,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=11.4+ (r988)
+set WSUSOFFLINE_VERSION=11.4+ (r989)
 title %~n0 %*
 echo Starting WSUS Offline Update (v. %WSUSOFFLINE_VERSION%) at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
